b5b6e6f2d53545c55645edb126d10aa4e06fd4a1
jose6alejandro <joseacastrorosales@gmail.com>
2019-12-29 01:37:30 -0400

    pr1-ula/quiz

---
+++ Biblioteca PR1-ULA/Quiz/02quiz.c
@@ -0,0 +1,42 @@
+#include "pr1-ula.h"
+
+void procesarMatriz(Entero matriz[3][2]);
+
+int main(){
+
+   Entero matriz[3][2] = {{20,  6},
+                          {11,  3},
+                          { 6, 19}};
+
+   procesarMatriz(matriz);                       
+
+   return 0;
+}
+
+               /* QUIZ 2 */
+
+void procesarMatriz(Entero matriz[3][2]){
+  
+   Entero sumaPar = 0;
+   Entero i, j;
+
+   for (i = 0; i < 3; i++){     
+      for (j = 0; j < 2; j++){
+
+         if (matriz[i][j] == 6){
+            imprimirCaracter('a');
+         }
+         if (matriz[i][j] != 6){
+            imprimirEntero(matriz[i][j]);
+         }
+         espaciar(1);
+         
+         if((matriz[i][j] % 2 == 0)){
+            sumaPar = matriz[i][j] + sumaPar; 
+         }
+      } 
+      saltar(1);
+   }
+   
+   imprimirEntero(sumaPar);
+}
