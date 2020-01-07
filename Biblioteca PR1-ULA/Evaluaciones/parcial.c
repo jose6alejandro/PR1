d6864db4c7ad175fa858d3b3877f9fab40da17a8
jose6alejandro <joseacastrorosales@gmail.com>
2020-01-07 19:42:37 -0400

    .

--- Biblioteca PR1-ULA/Evaluaciones/parcial.c
+++ Biblioteca PR1-ULA/Evaluaciones/parcial.c
@@ -35,7 +35,7 @@ Entero tiempoAsegundos(Entero hora, Entero minutos, Entero segundos){
 Entero mayor(Entero matriz[3][3]){
     
     Entero i, j;
-    Entero nroMayor = 0;
+    Entero nroMayor = matriz[0][0];
 
     for (i = 0; i < 3; i++){
         for (j = 0; j < 3; j++){
